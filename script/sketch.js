f84616d0a3187e6f0f93219941243c3a302ffb20
yuvvii <143991614+yuvvii@users.noreply.github.com>
2023-12-18 03:07:11 +0900

    1

--- script/sketch.js
+++ script/sketch.js
@@ -1,33 +1,51 @@
 let particles = [];
 const oWidth = 800;
 const oHeight = 700;
-let rectWidth = 700; // 네모의 가로 길이
-let rectHeight = 600; // 네모의 세로 길이
+let rectWidth = (oWidth / 800) * 700; // 네모의 가로 길이
+let rectHeight = (oWidth / 800) * 550; // 네모의 세로 길이
+
+let aShapeX; // A 도형의 x 좌표
+let aShapeY; // A 도형의 y 좌표
 
 function setup() {
   setCanvasContainer('canvas', oWidth, oHeight, true);
+
+  // A 도형 초기 설정
+  aShapeX = width / oWidth + 40 * (width / oWidth);
+  aShapeY = (540 * height) / oHeight;
 }
 
 function draw() {
   background('#FFEE72');
 
-  let activeParticles = 0; // 투명도가 0이 아닌 파티클 개수
+  let activeParticles = 0;
 
   // 네모 그리기
-  fill('beige'); // 네모의 채우기 색 설정
-  stroke('#FFC272');
+  fill('#FFEDDD');
   noStroke();
 
+  // 캔버스 비율 적용
   rectMode(CENTER);
-  rect(width / 2, height / 2 - 50, rectWidth, rectHeight, 20);
+  let canvasRatio = min(width / oWidth, height / oHeight);
+  let yOffset = 75 * (height / oHeight); // 높이에 비례한 값을 계산
+  rect(
+    width / 2,
+    height / 2 - 0.5 * yOffset,
+    rectWidth * canvasRatio,
+    rectHeight * canvasRatio,
+    20
+  );
 
   // 생성된 particle들을 그림
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].display();
 
-    // 일정 시간이 지나면 투명도를 줄여서 사라지게 함
-    if (millis() - particles[i].startTime > 3000) {
+    if (particles[i].color.levels[3] >= 80) {
+      activeParticles++;
+    }
+
+    if (millis() - particles[i].startTime > 2000) {
       particles[i].color.levels[3] -= 2;
       particles[i].color.levels[3] = constrain(
         particles[i].color.levels[3],
@@ -36,14 +54,201 @@ function draw() {
       );
     }
 
-    // 투명도가 0이 되면 배열에서 제거
     if (particles[i].color.levels[3] === 0) {
       particles.splice(i, 1);
     }
   }
+
+  // 마우스가 캔버스 내부에 있는 경우에만 새로운 파티클 생성
+  if (isMouseInsideCanvas()) {
+    if (random() > 0.5) {
+      let particle = new CustomParticle(
+        constrain(
+          mouseX,
+          width / 2 - rectWidth / 2 + 30 * canvasRatio,
+          width / 2 + rectWidth / 2 - 30 * canvasRatio
+        ),
+        constrain(
+          mouseY,
+          height / 2 - rectHeight / 2,
+          height / 2 + rectHeight / 2 - 90
+        )
+      );
+      particles.push(particle);
+    }
+  }
+
+  // 말풍선
+  for (let i = 0; i < 10; i++) {
+    let x = (182 + i * 61) * canvasRatio;
+    let y = aShapeY * canvasRatio;
+    drawCircle2(
+      (182 + i * 61) * canvasRatio,
+      (aShapeY + 160) * canvasRatio,
+      18 * canvasRatio
+    );
+  }
+
+  fill('#B6DBEF');
+  let ellipseCoords = [
+    { x: 670, y: 730 + 10 - 30 },
+    { x: 650, y: 730 - 15 - 30 },
+    { x: 685, y: 730 + 2 - 30 },
+    { x: 650, y: 730 + 10 - 30 },
+    { x: 640, y: 730 + 5 - 30 },
+    { x: 667, y: 730 - 15 - 30 },
+  ];
+
+  for (let coord of ellipseCoords) {
+    let x = coord.x * canvasRatio + 40;
+    let y = coord.y * canvasRatio - 70 * canvasRatio; // 수정된 부분
+    let diameter = 32 * canvasRatio;
+    ellipse(x, y, diameter, diameter);
+  }
+
+  // A 도형 그리기
+  noStroke();
+  fill('#FFEE72');
+  rect(
+    (aShapeX - 300) * canvasRatio,
+    aShapeY * canvasRatio,
+    600 * canvasRatio,
+    40 * canvasRatio
+  );
+
+  fill('#4E1D00');
+  ellipse(
+    (aShapeX + 30) * canvasRatio,
+    (aShapeY - 20) * canvasRatio,
+    20 * canvasRatio
+  );
+  ellipse(
+    (aShapeX - 30) * canvasRatio,
+    (aShapeY - 20) * canvasRatio,
+    20 * canvasRatio
+  );
+  ellipse(
+    (aShapeX - 40) * canvasRatio,
+    (aShapeY - 10) * canvasRatio,
+    20 * canvasRatio
+  );
+  ellipse(
+    (aShapeX + 40) * canvasRatio,
+    (aShapeY - 10) * canvasRatio,
+    20 * canvasRatio
+  );
+  ellipse(
+    (aShapeX - 50) * canvasRatio,
+    (aShapeY + 1) * canvasRatio,
+    15 * canvasRatio
+  );
+  ellipse(
+    (aShapeX + 50) * canvasRatio,
+    (aShapeY + 1) * canvasRatio,
+    15 * canvasRatio
+  );
+
+  fill('#FFCAAA');
+  ellipse(
+    aShapeX * canvasRatio,
+    (aShapeY + 3) * canvasRatio,
+    60 * canvasRatio,
+    55 * canvasRatio
+  );
+
+  fill('#4E1D00');
+  arc(
+    aShapeX * canvasRatio,
+    aShapeY * canvasRatio,
+    65 * canvasRatio,
+    62 * canvasRatio,
+    160,
+    0.3
+  );
+  ellipse(
+    (aShapeX + 5) * canvasRatio,
+    (aShapeY + 10) * canvasRatio,
+    4 * canvasRatio,
+    6 * canvasRatio
+  );
+  ellipse(
+    (aShapeX - 5) * canvasRatio,
+    (aShapeY + 10) * canvasRatio,
+    4 * canvasRatio,
+    6 * canvasRatio
+  );
+
+  fill('#FF4D4D');
+  ellipse(
+    (aShapeX + 18) * canvasRatio,
+    (aShapeY + 15) * canvasRatio,
+    10 * canvasRatio,
+    8 * canvasRatio
+  );
+  ellipse(
+    (aShapeX - 18) * canvasRatio,
+    (aShapeY + 15) * canvasRatio,
+    10 * canvasRatio,
+    8 * canvasRatio
+  );
+
+  // A 도형 이동 로직
+  let targetX = map(
+    activeParticles,
+    0,
+    300,
+    160 * canvasRatio,
+    800 * canvasRatio
+  );
+  aShapeX += (targetX - aShapeX) * 0.05;
+
+  aShapeX = constrain(aShapeX, 160 * canvasRatio, 800 * canvasRatio);
+
+  // // 스케치북 구멍
+  // for (let i = 0; i < 11; i++) {
+  //   drawCircle(80 + i * 60 * canvasRatio, 80 * canvasRatio, 30 * canvasRatio);
+  // }
+
+  //가로로 길게 용수철처럼 구불거리는 선 그리기
+  drawWavyLine(
+    90 * canvasRatio,
+    30 * canvasRatio,
+    626 * canvasRatio,
+    0.1 * canvasRatio,
+    15 * canvasRatio
+  );
 }
 
-console.log('파티클 개수:', particles.length);
+function drawCircle(x, y, diameter) {
+  fill('#FFC272');
+  noStroke(0);
+  ellipse(x, y, diameter, diameter);
+}
+
+function drawCircle2(x, y, diameter) {
+  fill('#B6DBEF');
+  noStroke(0);
+  ellipse(x, y, diameter, diameter);
+}
+
+function drawWavyLine(x, y, length, frequency, amplitude) {
+  stroke('#90D9E8');
+  noFill();
+  strokeWeight(15);
+
+  beginShape();
+  for (let i = 0; i < length; i += 1) {
+    let xpos = x + i;
+    let ypos = y + cos(i * frequency) * amplitude;
+    vertex(xpos, ypos);
+  }
+  endShape();
+}
+
+// 마우스가 캔버스 내부에 있는지 확인하는 함수
+function isMouseInsideCanvas() {
+  return mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height;
+}
 
 // CustomParticle 클래스 정의
 class CustomParticle {
@@ -58,6 +263,7 @@ class CustomParticle {
     );
     this.numShapes = 6; // 도형의 개수
     this.startTime = millis(); // 생성 시간 저장
+    this.baseSize = 11; // 기본 크기
   }
 
   update() {
@@ -79,11 +285,18 @@ class CustomParticle {
     translate(this.x, this.y);
     rotate(radians(90));
 
-    // noStroke();
+    // 캔버스 크기에 따라 비율 계산
+    let sizeRatio = (width / oWidth) * 0.8;
+    let shapeGap = 0.000001 * sizeRatio; // 도형 간격
 
     for (let i = 0; i < this.numShapes; i++) {
       rotate(radians(60));
-      ellipse(8, 0, 12, 10);
+      ellipse(
+        (this.baseSize + shapeGap * i) * sizeRatio,
+        0,
+        this.baseSize * sizeRatio,
+        10 * sizeRatio
+      );
     }
     pop();
   }
@@ -94,26 +307,31 @@ function isInsideRect(x, y, rectX, rectY, rectWidth, rectHeight) {
     x > rectX - rectWidth / 2 &&
     x < rectX + rectWidth / 2 &&
     y > rectY - rectHeight / 2 &&
-    y < rectY + rectHeight / 2
+    y < rectY + rectHeight / 2 - 20
   );
 }
-
 function mouseMoved() {
-  // 마우스 움직일 때마다 새로운 파티클 생성
-  if (random() > 0.5) {
+  // 마우스가 캔버스 내부에 있는 경우에만 새로운 파티클 생성
+  if (isMouseInsideCanvas() && random() > 0.5) {
+    let constrainedY = constrain(
+      mouseY,
+      height / 2 - rectHeight / 2 + 50,
+      height / 2 + rectHeight / 2 - 230
+    );
+
+    if (constrainedY !== mouseY) {
+      return; // y 좌표가 제한되었다면 함수를 빠져나감
+    }
+
     let particle = new CustomParticle(
       constrain(
         mouseX,
         width / 2 - rectWidth / 2 + 30,
         width / 2 + rectWidth / 2 - 30
       ),
-      constrain(
-        mouseY,
-        height / 2 - rectHeight / 2 - 20,
-        height / 2 + rectHeight / 2 - 90
-      )
+      constrainedY
     );
     particles.push(particle);
-    mouse.pixelRatio = (pixelDensity() * width) / canvasWidth;
+    mouse.pixelRatio = (pixelDensity() * width) / oWidth;
   }
 }
